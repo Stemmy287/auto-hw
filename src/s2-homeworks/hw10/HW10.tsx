d1a1f6a953512bb0c5ceed2d9d5f74b3f74413f6
Stemmy287 <steem287@gmail.com>
2022-12-14 11:56:26 +0300

    lesson 10

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -5,6 +5,7 @@ import {loadingAC} from './bll/loadingReducer'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import s2 from '../../s1-main/App.module.css'
 import {Loader} from './Loader'
+import {setFips} from "crypto";
 
 /*
 * 1 - в файле loadingReducer.ts дописать типы и логику
@@ -15,11 +16,15 @@ import {Loader} from './Loader'
 
 const HW10 = () => {
     // useSelector, useDispatch // пишет студент
-    const isLoading = false
+    const dispatch = useDispatch()
+    const isLoading = useSelector((state: AppStoreType) => state.loading.isLoading)
 
     const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
         // dispatch
-
+            dispatch(loadingAC(true))
+            setTimeout(() => {
+                dispatch(loadingAC(false))
+            }, 1500)
         // setTimeout
     }
 
